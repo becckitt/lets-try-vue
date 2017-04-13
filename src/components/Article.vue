d4249c7b27a9ca3fbd18d8217eebc755cbcfd43a
Becca Barton <becca.barton@voxmedia.com>
2017-04-13 15:15:28 -0400

    first version of article component

---
+++ src/components/Article.vue
@@ -0,0 +1,100 @@
+<template>
+  <section class="content">
+    <article>
+      <section class="title">
+        <h1>{{ title }}</h1>
+        <p>{{ subtitle }}</p>
+      </section>
+      <section class="body">
+        <img src="https://cdn0.vox-cdn.com/thumbor/Io-IxH5Py99wjB0UZ7xJeX647_U=/0x0:6016x4016/2820x1586/filters:focal(2527x1527:3489x2489):format(webp)/cdn0.vox-cdn.com/uploads/chorus_image/image/54109291/EM_Comestics_2589_PILAR.0.jpg" alt="">
+        <p>“I completely disappeared. I just left. I stopped uploading to <a href="https://www.youtube.com/user/MichellePhan">YouTube</a>. I’m an Aries so I’m either very hot or very cold,” says Michelle Phan, who stopped by the Racked offices on a recent whirlwind press junket to promote the launch of her Em cosmetics line. It’s not the first time she’s launched Em, however. </p>
+
+        <p>Phan, who is almost 30, is one of YouTube’s biggest success stories and arguably one of the most popular and recognizable so-called beauty gurus on the platform, with almost 9 million subscribers and well over a billion views of her videos. Then there’s <a href="https://www.ipsy.com/">Ipsy</a>, her beauty sampling company, which is worth a reported <a href="https://www.forbes.com/sites/natalierobehmed/2015/10/05/how-michelle-phan-built-a-500-million-company/#18ef3a698c4a">$500 million</a>.</p>
+
+        <p>Phan is successful in her field, if not at the very top of it. At the beginning of 2016, she landed on the covers of both <a href="https://www.forbes.com/pictures/mkl45ehmdj/michelle-phan-27/#3f1a54542065"><em>Forbes</em></a> and <a href="http://www.nylon.com/articles/michelle-phan-february-cover-star"><em>Nylon</em></a>, lauded for her success both online and off. She should have been damn pleased with herself at that point. But that’s not how she felt.</p>
+
+        <p>“I peaced out because I think I was going through depression. I don’t know because I didn’t go to a hospital or anything or get diagnosed, but I was taking a few quizzes online and I felt really sad every day,” Phan says. Of her dual covers, she muses: “You would think, <em>This achievement is great. You should be happy.</em> But I wasn’t. I was waking up feeling so broken. I didn’t know why.”</p>
+
+        <p>It likely had something to do with Em, the makeup line that L'Oréal pitched to her, produced, and then launched in 2013, after almost three years of development with Phan’s close involvement. It was not well-received and didn’t sell well, garnering blisteringly <a href="https://www.makeupalley.com/product/showreview.asp/ItemId=165499/THE-LIFE-PALETTE-Party-Life/em-cosmetics/Palettes">critical reviews</a> from her fans, who complained about the price point and levied accusations that she had “<a href="https://www.reddit.com/r/MakeupAddiction/comments/1kijen/my_response_to_michelle_phans_em_cosmetics_price/">sold out</a>.” By the end of 2015, ominous reports were coming from industry trade papers like <a href="http://wwd.com/beauty-industry-news/beauty-features/loreal-usa-michelle-phan-part-ways-em-michelle-phan-heads-to-ipsy-com-10259153/"><em>WWD</em></a> suggesting that the price, which was upwards of $50 for some products, was indeed too high for Phan’s demographic. Then Phan <a href="http://fashionista.com/2015/10/michelle-phan-ipsy">announced</a> that she would be buying the line back from L'Oréal.</p>
+
+        <p>In early 2016, Phan dropped everything and decided to travel the world for nine months, visiting Egypt, China, and Europe, among other places. “I think I reached that limit where [I thought], ‘You need to resolve all this, you need to figure it out, you need to heal yourself,  you need to reflect.</p>
+
+        <p>She deleted a bunch of videos from her channel and changed her profile picture to an ominous black box. Her fans noticed her silence online, posting worried comments like this one on <a href="https://www.youtube.com/watch?v=MXm7obi1yHM">her last video</a>, from nine months ago: “Michelle why is your picture black? I am worried about you...Please come back like once a month or something :-( I love you so much Michelle &lt;3 you were the reason I even joined YouTube.” She rarely even posted to social media. “People literally thought I was dead,” she laughs.</p>
+
+        <p>Phan admits she pretty much left everyone to fend for themselves. “My poor colleagues back home were really struggling because I wasn’t as around as much,” she says. “But I have a great team and they know exactly where my vision is and where I align. I don’t have kids yet and I’m not married. I can just wake up whenever. It was probably one of the most memorable experiences I ever had in my life, just waking up and not knowing what to do.”</p>
+
+        <p>By now, Phan’s <a href="http://mashable.com/2016/02/13/michelle-phan-starting-over/#mJFkfntJGSqh">origin story</a> is well-known, at least by those who care about beauty. Her parents were Vietnamese refugees. Her dad had a gambling problem; her parents eventually broke up and a strict stepdad entered the picture, then left a few years later. “The biggest impression I had growing up was that we were always poor,” Phan says.</p>
+
+        <p>Phan was going to become a doctor, but instead scraped together the money to go to art school, where she was given a Macbook, which ended up being serendipitous. It’s what she used to record and upload her <a href="https://www.youtube.com/watch?v=OB8nfJCOIeE">very first video</a>, in 2007, about her everyday makeup routine. It got 70,000 views within the first two weeks, and garnered a million in the first year, which was a big deal in those early days of YouTube. She quit her part-time waitressing job, then quit school after YouTube accepted her into its newly minted partners program. Her channel started making money — first 25 cents a day, then $50 a week. Lancôme came calling to offer her a gig helping them make tutorials. She worked for the brand for several years, and in the meantime blew up on YouTube with videos like her <a href="https://www.youtube.com/watch?v=J4-GRH2nDvw">Barbie tutorial</a>, which is still her most viewed at almost 66 million views, and a Lady Gaga <a href="https://www.youtube.com/watch?v=bHVOxhEpjp0">“Bad Romance” inspiration video.</a></p>
+
+        <p>Phan started working on Ipsy around 2010, and L'Oréal reached out about starting a beauty line with her around the same time. L'Oréal is known for acquiring and nurturing brands like Urban Decay and NYX, so Em was a startup experience for the company. “I don’t think me having my own makeup line was a dream I’ve always wanted,” she says. “It was an opportunity that L'Oréal brought to me, and I’m not gonna say no to that opportunity. I knew that it would be such an amazing experience building something like that.” Great opportunity aside, Phan was now involved in launching two startups simultaneously, and she shuttled between the East and West coasts for several years.</p>
+
+        <p>When Em imploded, Phan didn’t take it well, but now she views it as a second chance. “I was really hurt when the brand didn’t take off,” she says. “It just sucked. It was three years of my life gone, just like that. But I learned a lot from that and I think it was really important for me to fail. It was through that failure that I started to get more interested in the business aspect of everything.” Several times during the course of our conversation, she says, “I’m going to learn from all of these mistakes.”</p>
+
+        <p>Some of the mistakes are obvious with the benefit of hindsight. Since Em was launched in 2013, several indie makeup lines have popped up, like <a href="https://colourpop.com/">Colourpop</a> and <a href="https://www.makeupgeek.com/">Makeup Geek</a>, founded in 2012 by YouTuber <a href="https://www.forbes.com/sites/emmajohnson/2015/05/13/how-makeup-geek-ceo-marlena-stell-built-a-10m-business-on-youtube-and-honesty/#5592ff233796">Marlena Stell</a>, one of Phan’s contemporaries. This trend of social media-spawned beauty brands all ultimately led to the juggernaut known as Kylie Cosmetics. The winning strategy seems to be online personalities dabbling first in collaborations with established makeup brands: Kathleen Lights, for example, did collabs with indie brands <a href="https://colourpop.com/collections/kathleenlights">Colourpop</a>, <a href="https://www.ofracosmetics.com/pages/miamifever">Ofra</a>, <a href="http://www.revelist.com/beauty-news-/kathleen-lights-morphe-palette/5362">Morphe</a>, and <a href="https://www.makeupgeek.com/kathleen-lights-highligher-palette.html">Makeup Geek</a>. She is now launching <a href="http://www.glamour.com/gallery/kathleen-lights-kl-nail-polish-line-photos">her own line</a>, starting with nail polish. Jaclyn Hill had a sell-out collaboration with Becca as well as several other indie brands, and is <a href="http://www.glamour.com/story/jaclyn-hill-revealed-a-sneak-peek-at-her-makeup-line">rumored</a> to be launching her own line this year. The common strategy among indie beauty brands now is to tease the launch on social media for months, then drop products piecemeal — rather than releasing a full 200-product collection the old-school way, which is how the first Em line launched.</p>
+
+        <p>This is also how Phan is releasing the new Em, whose first products will appear April 17th on <a href="https://www.emcosmetics.com/">Em’s website</a>. She took a year to develop the logo, which features a geometric shape inspired by <a href="http://www.mathematicsmagazine.com/Articles/SacredGeometryPlatonicSolids.php#.WOJIexLytE4">sacred geometry and the platonic solids</a>. Phan tapped into her art background for this launch in several ways, and she is pretty unabashed about her spiritual journey, all of which culminated in the logo.</p>
+
+        <p>There will be only two categories — liquid eyeliners and creamy liquid lipsticks — and 10 products at launch. When Phan came to Racked’s offices, the first thing I noticed was how little makeup she was wearing. She says she’s given up on foundation completely because she’s convinced that 10 years of heavy makeup took a toll on her skin. Her own new stripped-down aesthetic inspired her line, but so did her followers. Phan uploaded a video (since deleted) asking her fans what products they’d like to see from her, and the most common request was for a liquid lipstick that wasn’t drying and uncomfortable to wear.</p>
+
+        <p>The result is Infinite Lip Cloud ($16), a cream formula in a tube that applies like a liquid lipstick with super opaque color, but doesn’t dry your lips out. It isn’t as long-lasting as a traditional liquid lipstick, but it wears into a lovely stain after a few hours. There are eight shades with names like Spanish Earth and Faded Clementine, which she says were inspired by favorite oil paint colors. The second product is the Illustrative Eyeliner Collection ($15), which features two different applicators: a marker tip and a skinny brush tip. Both are inspired by art pens. Phan recommends using the marker for bolder lines and the brush for precision.</p>
+
+        <p>Phan also tapped five “muses” to act as brand ambassadors, including her sister-in-law and fellow YouTuber <a href="https://www.youtube.com/user/Promise21111">Promise Phan</a>, who is known for her celebrity and character transformations. <a href="https://www.youtube.com/user/JkissaMakeup">Jessica Stanley</a>, <a href="https://www.youtube.com/user/RoxetteArisa">Roxette Arisa</a>, <a href="https://www.youtube.com/user/thegalsguide">Mariah Leonard</a>, and <a href="https://www.youtube.com/channel/UCRJdHfdCRI630Jj9PmZZByQ">Jade Simmone</a> make up the remaining muses; their subscriber counts range from 1,500 to over a million. Phan has long mentored up-and-coming creators via her <a href="http://ipsyos.com/">Ipsy OS program</a>, which has 10,000 members. The muse program is also a way to showcase her products via influencers who are still really into using makeup creatively — something Phan herself doesn’t do as much anymore.</p>
+
+        <p>After speaking to Phan for an hour, I noticed her palpable sense of relief about the decision to move behind the scenes and away from being in front of the camera. “Getting recognized in real life is always weird, because you never know when it’s gonna happen,” she says. “I can’t even tell you how many times I had to take pictures with people in the bathroom. It’s the most awkward thing.” she says. She won’t miss the commenter trolls either, one of whom wrote terrible things on her videos, then showed up at an event bearing a teddy bear for her. Even her mom and extended family get recognized now.</p>
+
+        <img src="https://cdn0.vox-cdn.com/thumbor/S7oHR_oB3SAkJX1zj4TxuogYriQ=/3000x0/filters:no_upscale()/cdn0.vox-cdn.com/uploads/chorus_asset/file/8291683/EM_Comestics_2936_PILAR.jpg" alt="">
+
+        <p>Phan also knows that YouTubers and Instagram beauty gurus are in a challenging place right now, because there is so much competition. She discourages her mentees from doing gimmicky videos (like using a <a href="http://www.racked.com/2017/2/8/14491942/silisponge-silicone-makeup-sponge-review">condom to apply makeup</a>), but understands why they do. “It’s shock value,” she says. “That’s the only way for them to get views. When I started there were probably fewer than five people [doing makeup videos], and now you probably have 50,000 or maybe more globally. I’ve mentored a few people who started doing these gimmicky things and I told them, ‘You’re going to burn out very fast and you’re not going to have a loyal community,’ and sure enough they blow up and then lose steam really fast.”</p>
+
+        <p>For now, Phan is relishing what she calls her second chance. “I’m actually happy that Em didn’t do well, because I’m happy that I own it,” she says. “It is my makeup brand, it’s not L'Oréal’s makeup brand. It’s not for me; it’s for my viewers who have been there since the beginning.”</p>
+
+        <p>As for whether or not this new venture will go viral, Phan laughs at the term. “Those days are over. Viral back then was ‘<a href="https://www.youtube.com/watch?v=9bZkp7q19f0">Gangnam Style</a>,’” she says. “When I meet with people who are trying to sell me things like, ‘Yeah, we’re gonna make it viral!’ it’s like, cringe, dude. No, you’re not going to make it viral. Viral chooses you.”</p>
+
+      </section>
+    </article>
+  </section>
+
+</template>
+
+<script>
+  export default {
+    name: 'article',
+    data () {
+      return {
+        title: 'The Rebirth of YouTube Beauty Pioneer Michelle Phan',
+        subtitle: 'After 10 years and over a billion views, she stopped posting and “peaced out.”'
+      }
+    }
+  }
+  
+</script>
+
+<style scoped>
+  h1 {
+    font-weight: bold;
+    font-size: 2.5rem;
+    margin-bottom: 10px;
+  }
+
+  a {
+    color: #42b983;
+  }
+
+  img {
+    max-width: 100%;
+  }
+
+  p {
+    max-width: 800px;
+    width: 100%;
+    margin: 10px auto;
+  }
+
+  .title {
+    text-align: center;
+    max-width: 800px;
+    width: 100%;
+    margin: 0 auto 40px auto;
+  }
+</style>
