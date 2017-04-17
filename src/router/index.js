c12941c6e2df58cd35256fd887535496921df0fd
Becca Barton <becca.barton@voxmedia.com>
2017-04-17 16:28:53 -0400

    add lazy load functionality for ad

--- src/router/index.js
+++ src/router/index.js
@@ -2,9 +2,15 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import VueResource from 'vue-resource'
 import Article from '@/components/Article'
+import VueLazyLoad from 'vue-lazyload'
 
 Vue.use(VueRouter)
 Vue.use(VueResource)
+
+Vue.use(VueLazyLoad, {
+  preLoad: 1.2,
+  lazyComponent: 'Ad'
+})
 
 export default new VueRouter({
   routes: [
